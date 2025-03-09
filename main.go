2cc080742700571d12c774991baf8fa8a52b2fb3
Mario Hernandez <mhmario963@gmail.com>
2025-03-08 19:17:18 -0500

    plan out my strategy.

--- main.go
+++ main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+type user struct {
+	wins        int
+	currentMove int
+}
 
 func main() {
-	fmt.Println("Hello world")
+	numRounds := 3
+	numWins := numRounds/2 + 1
+
+	fmt.Println(numRounds)
+	fmt.Println(numWins)
+
+	user1 := &user{}
+	user2 := &user{}
+
+	for user1.wins < numWins && user2.wins < numWins {
+		// user 1 will get input from stdin.
+		// then user2 will get input based on the outcome of random generator.
+
+		// I'll compare to see who won, then round, it's possible there will be a tie.
+
+		// I want to "draw" the round outcome with emojis to make it look cool
+	}
+
+	// here I'll print out who one
+	// maybe i can have a "wanna keep going?" button.
 }
