baafa568a1027df99812f7b01e089a2a0aa81e4b
Mario Hernandez <mhmario963@gmail.com>
2025-03-09 23:12:39 -0400

    return the names of the user.

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"github.com/mariohdez/rockpaperscissors/internal/game"
 	"github.com/mariohdez/rockpaperscissors/internal/net/protocol"
+	"github.com/mariohdez/rockpaperscissors/internal/user"
 	"log"
 	"net"
 	"sync"
@@ -56,13 +57,26 @@ func orchestrateNewGame(player1Conn, player2Conn net.Conn) {
 	defer player1Conn.Close()
 	defer player2Conn.Close()
 
-	getNames(player1Conn, player2Conn)
+	connToName := getNames(player1Conn, player2Conn)
+	player1Name, ok := connToName[player1Conn]
+	if !ok {
+		log.Println("Could not find player1's name:", player1Conn)
+		return
+	}
+	player2Name, ok := connToName[player2Conn]
+	if !ok {
+		log.Println("Could not find player2's name:", player2Conn)
+		return
+	}
 
-	game := game.NewMatch(3, nil, nil, nil, nil, nil)
-	game.Start()
+	_ = game.NewMatch(3, &user.Player{
+		Name: player1Name,
+	}, &user.Player{
+		Name: player2Name,
+	}, nil, nil, nil)
 }
 
-func getNames(player1Conn, player2Conn net.Conn) {
+func getNames(player1Conn, player2Conn net.Conn) map[net.Conn]string {
 	var wg sync.WaitGroup
 	connToName := make(map[net.Conn]string)
 	connToNameLock := sync.Mutex{}
@@ -87,6 +101,7 @@ func getNames(player1Conn, player2Conn net.Conn) {
 			return
 		}
 
+		log.Printf("[%s] Got name: %s", playerConn.RemoteAddr(), data)
 		connToNameLock.Lock()
 		connToName[playerConn] = data
 		connToNameLock.Unlock()
@@ -99,4 +114,5 @@ func getNames(player1Conn, player2Conn net.Conn) {
 	wg.Wait()
 
 	log.Println("Got both players names")
+	return connToName
 }
